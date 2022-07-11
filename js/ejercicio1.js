8234e6b5f50734d577c5f9da169a2194a4bb1ce3
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-07-11 08:38:37 -0300

    guardamos este primer ejercicio del tp numero 2 de javascipt

---
+++ js/ejercicio1.js
@@ -0,0 +1,9 @@
+let Edad = prompt("Ingrese su edad");
+
+if (Edad >= 18){
+    document.write("ya puede conducir")
+}
+
+if(isNaN(Edad)){
+    document.write("El valor ingresado debe ser numero");
+    }
