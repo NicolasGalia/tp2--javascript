4a272ba8a378206957bfd3a1b6e782b63cb84135
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-07-17 22:27:44 -0300

    terminamos el ejercicio numero 16

---
+++ js/ejercicio16.js
@@ -0,0 +1,8 @@
+
+
+let texto = prompt("Escriba un texto, lo devolveremos como 'otxet'")
+
+for(let i = texto.length - 1 ; i >= 0 ; i--){
+    let Alreves = texto[i];
+    document.write(Alreves);
+}
