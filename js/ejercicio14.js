1d17714f55616a9c8ab7d24a322cf536ac6cbbef
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-07-17 22:12:37 -0300

    terminamos el ejercicio numero 14

---
+++ js/ejercicio14.js
@@ -0,0 +1,6 @@
+// Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
+
+let cadenaDeTexto = prompt("escriba una cadena de texto");
+for (i = 0; i < cadenaDeTexto.length; i++) {
+    document.write(cadenaDeTexto [i] + " - ")
+}
